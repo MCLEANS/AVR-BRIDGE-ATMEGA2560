646d94b45452667634f9e9b2bb1d526d10be0efb
MCLEANS <jackagwaya@gmail.com>
2020-07-10 09:14:45 +0300

    Defined method to obtain input pin status

--- GPIO.cpp
+++ GPIO.cpp
@@ -8,6 +8,13 @@ void GPIO::set_input(volatile uint8_t* DIRECTION_REGISTER, uint8_t PIN){
     *DIRECTION_REGISTER &= ~(1 << PIN);
 }
 
+bool GPIO::input_status(volatile uint8_t* PORT_REGISTER, uint8_t PIN){
+    bool status = false;
+    status = (*PORT_REGISTER & (1 << PIN));
+    
+    return status;
+}
+
 void GPIO::set_output(volatile uint8_t* DIRECTION_REGISTER, uint8_t PIN){
     *DIRECTION_REGISTER |= (1 << PIN);
 }
